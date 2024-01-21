17d02dcabee21dd421536e7c413e1ba84e3dbefd
Awais-Ansarii <awaisansari1502@gmail.com>
2024-01-21 18:45:26 +0530

    created routes for home and category pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
-import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "./components/Header/Header"
+import Home from "./components/Home/Home"
+import Footer from  "./components/Footer/Footer"
+import Category from "./components/Category/Category"
+import SingleProduct from "./components/SingleProduct/SingleProduct"
+// import Newsletter from "./components/Newsletter/Newsletter"
+
+
+
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/category/:id" element={<Category />} />
+      <Route path="/product/:id" element={<SingleProduct />} />
+    </Routes>
+  </BrowserRouter>
+  )
+  
 };
 
 export default App;
