5c540a180a0f90e33ef6bfd663f1266dfb61640c
Awais-Ansarii <awaisansari1502@gmail.com>
2024-01-21 22:36:40 +0530

    created a category section

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,14 +1,18 @@
 import "./Home.scss";
 import Banner from "./Banner/Banner";
+import Category from "./Category/Category";
 const Home = () => {
   return (
-    <div className="home">
-      
-      <Banner/>
+    <div className="">
+      <Banner />
+      <div className="main-content">
+        <div className="layout">
+          <Category />
+        </div>
+      </div>
       Homee
-    
     </div>
-  )
+  );
 };
 
 export default Home;
