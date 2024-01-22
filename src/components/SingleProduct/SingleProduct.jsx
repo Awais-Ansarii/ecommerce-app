16185b2a4671c76d9d3bf8e51ed4c486ebd13bed
Awais-Ansarii <awaisansari1502@gmail.com>
2024-01-22 11:46:49 +0530

    created single product info page and made that responsiveI

--- src/components/SingleProduct/SingleProduct.jsx
+++ src/components/SingleProduct/SingleProduct.jsx
@@ -1,6 +1,77 @@
+import { useContext, useState } from "react";
+
+import {
+  FaFacebookF,
+  FaTwitter,
+  FaInstagram,
+  FaLinkedinIn,
+  FaPinterest,
+  FaCartPlus,
+} from "react-icons/fa";
+
+import productImg from "../../assets/products/watch-prod-3.webp";
 import "./SingleProduct.scss";
+
 const SingleProduct = () => {
-    return <div>Single Product</div>;
+  const [quantity, setQuantity] = useState(1);
+
+  const decrement = () => {
+    setQuantity((prevState) => {
+      if (prevState === 1) return 1;
+      return prevState - 1;
+    });
+  };
+  const increment = () => {
+    setQuantity((prevState) => prevState + 1);
+  };
+
+  return (
+    <div className="single-product-main-content">
+      <div className="layout">
+        <div className="single-product-page">
+          <div className="left">
+            <img src={productImg} />
+          </div>
+
+          <div className="right">
+            <span className="name">product-title </span>
+            <span className="price"> Price &#8377; 500</span>
+            <span className="desc">product-description</span>
+
+            <div className="cart-buttons">
+              <div className="quantity-buttons">
+                <span onClick={decrement}>-</span>
+                <span>{quantity}</span>
+                <span onClick={increment}>+</span>
+              </div>
+              <button className="add-to-cart-button">
+                <FaCartPlus size={20} />
+                ADD TO CART
+              </button>
+            </div>
+
+            <span className="divider" />
+            <div className="info-item">
+              <span className="text-bold">
+                Category:{" "}
+                <span>Headphones</span>
+              </span>
+              <span className="text-bold">
+                Share:
+                <span className="social-icons">
+                  <FaFacebookF size={16} />
+                  <FaTwitter size={16} />
+                  <FaInstagram size={16} />
+                  <FaLinkedinIn size={16} />
+                  <FaPinterest size={16} />
+                </span>
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SingleProduct;
